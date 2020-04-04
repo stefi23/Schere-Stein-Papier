efcdbb680ffff012b05908b44e8af6c9214b2791
stefi23 <stefania.rosca2309@gmail.com>
2020-04-04 23:37:15 +0200

    Updates

---
+++ precache-manifest.9eaacd6ea3ced89f137cb48ce4220143.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9895afd19da2e52c3e4df04caf358804",
+    "url": "/Schere-Stein-Papier/index.html"
+  },
+  {
+    "revision": "8e64499a83400d7346fb",
+    "url": "/Schere-Stein-Papier/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5c3ba0e8a1a7765d5c1e",
+    "url": "/Schere-Stein-Papier/static/js/2.8fc607f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Schere-Stein-Papier/static/js/2.8fc607f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e64499a83400d7346fb",
+    "url": "/Schere-Stein-Papier/static/js/main.868e7565.chunk.js"
+  },
+  {
+    "revision": "e1b585953c0fb54a1b7d",
+    "url": "/Schere-Stein-Papier/static/js/runtime-main.e00cc359.js"
+  }
+]);
