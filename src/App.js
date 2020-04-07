2e90512f6f07984fd866b25589c080fed565f275
stefi23 <stefania.rosca2309@gmail.com>
2020-04-07 19:20:49 +0200

    Design updates

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      element: ["paper", "scissors", "rock"],
+      element: ["✋", "✌️", "✊"],
       computer: "",
       player: "",
       winner: "unknown",
@@ -35,28 +35,28 @@ class App extends React.Component {
       return Math.floor(Math.random() * (max - min)) + min;
     }
     let index = getRandomInt();
-    if (index === 0) return "paper";
-    else if (index === 1) return "scissors";
-    else return "rock";
+    if (index === 0) return "✋";
+    else if (index === 1) return "✌️";
+    else return "✊";
   };
 
   getWinner = (player, computer) => {
     if (player === computer) {
       return "tie";
-    } else if (player === "rock") {
-      if (computer === "scissors") {
+    } else if (player === "✊") {
+      if (computer === "✌️") {
         return "player";
       } else {
         return "computer";
       }
-    } else if (player === "scissors") {
-      if (computer === "paper") {
+    } else if (player === "✌️") {
+      if (computer === "✋") {
         return "player";
       } else {
         return "computer";
       }
-    } else if (player === "paper") {
-      if (computer === "rock") {
+    } else if (player === "✋") {
+      if (computer === "✊") {
         return "player";
       } else {
         return "computer";
@@ -68,6 +68,15 @@ class App extends React.Component {
     this.setState({
       winner: "unknown",
     });
+  };
+
+  resetGame = () => {
+    this.setState({
+      computerScore: 0,
+      playerScore: 0,
+      tie: 0,
+    });
+    this.playGame();
   };
 
   getScore = (winner) => {
@@ -88,19 +97,21 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
+      <div className="App ">
         <header className="App-header">
-          <h1>Rock, Paper, Scissors!</h1>
-          <div className="container">
-            <p>Play against the computer! Press any button to start.</p>
+          <div className="p-3 box">
+            <h1>Rock, Paper, Scissors!</h1>
 
             {this.state.winner === "unknown" ? (
               <div className="row">
+                <div className="container ">
+                  <p>Play against the computer. Press any button to start.</p>
+                </div>
                 <div className="col-12">
                   <button
                     onClick={this.handleChoice}
                     name="user"
-                    value={"rock"}
+                    value={"✊"}
                     className="m-2 buttonGrayRounded"
                   >
                     ✊
@@ -108,7 +119,7 @@ class App extends React.Component {
                   <button
                     onClick={this.handleChoice}
                     name="user"
-                    value={"paper"}
+                    value={"✋"}
                     className="m-2 buttonGrayRounded"
                   >
                     ✋
@@ -117,7 +128,7 @@ class App extends React.Component {
                   <button
                     onClick={this.handleChoice}
                     name="user"
-                    value={"scissors"}
+                    value={"✌️"}
                     className="m-2 buttonGrayRounded"
                   >
                     ✌️
@@ -125,26 +136,51 @@ class App extends React.Component {
                 </div>
               </div>
             ) : (
-              <div className="row">
-                <div className="col-6 text-center">
-                  <p>{this.state.player}</p>
+              <div className="row p-3 m-3">
+                <div className="col-4 text-center">
+                  <p className="m-2">
+                    <span className="buttonBoxPink"> {this.state.player}</span>
+                  </p>
                 </div>
-                <div className="col-6 text-center">
-                  <p>{this.state.computer}</p>
+                <div className="col-4 text-center d-flex align-items-center justify-content-center ">
+                  <p className="mb-0">
+                    {" "}
+                    {this.state.winner === "tie"
+                      ? "You have a tie"
+                      : this.state.winner + " won!"}
+                  </p>
                 </div>
-                <div className="col-12 text-center">
-                  <p>{this.state.winner} won!</p>
+                <div className="col-4 text-center">
+                  <p className="m-2 ">
+                    <span className="buttonBox"> {this.state.computer}</span>
+                  </p>
                 </div>
               </div>
             )}
           </div>
-          <button className="btn btn-dark m-2" onClick={this.playGame}>
-            Play again!
-          </button>
-          <div className="row">
+          <div className="row mt-2">
             <div className="col-12">
-              Score
-              <table id="scoreboard" align="center">
+              <button
+                className="buttonPink m-2 border rounded"
+                onClick={this.playGame}
+              >
+                Play again!
+              </button>
+              <button
+                className="buttonPink m-2 border rounded"
+                onClick={this.resetGame}
+              >
+                Reset Game
+              </button>
+            </div>
+          </div>
+          <div className="row mt-4">
+            <div className="col-12">
+              <table
+                id="scoreboard"
+                align="center"
+                className="table table-dark"
+              >
                 <tr>
                   <td> Player </td>
                   <td width="30"> &nbsp; </td>
